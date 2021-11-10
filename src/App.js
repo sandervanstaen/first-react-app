76a2902da67b03cbae35ae7d3341ef6d6cd98eed
Sander Vanstaen <vanstaensander@gmail.com>
2021-11-10 15:13:04 +0100

    index.html link update

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1>Search Monsters</h1>
         <SearchBox
           placeholder="search monsters"
           HandleSearch={this.HandleSearch}
